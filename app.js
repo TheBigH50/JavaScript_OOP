05ec65d632063e14f483e17b59e5443c1bf7eaca
TheBigH50 <HFangmeier@outlook.com>
2022-08-22 13:26:23 -0500

    Exe1

--- app.js
+++ app.js
@@ -2,3 +2,25 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+//Exe1
+
+class Person{
+    constructor(name, pets, residence, hobbies) {
+    this.name = name;
+    this.pets = pets;
+    this.residence = residence;
+    this.hobbies = hobbies;
+    };
+   
+    addHobby(hobby) {
+return this.hobbies + hobby
+    };
+    removeHobby(hobby) {
+        return this.hobbies - hobby
+    };
+    greeting(greet) {
+        console.log(`${greet} fellow person`)
+    }
+};
+
